4642decbf9c748048bae9f05db1577af16b1d07c
Soumaya <selmansouri@appliedinvention.com>
2025-09-13 18:35:43 -0700

    integrated pygame displaying map

--- A1_Flatlands/main.py
+++ A1_Flatlands/main.py
@@ -1,13 +1,65 @@
 import env_handler as Enviornment
 import entity
+import numpy as np
+import pygame
+
+def draw_grid(screen_w, screen_h, cell_size, screen):
+    # --- Draw grid lines ---
+    # Vertical lines
+    for x in range(0, screen_w, cell_size):
+        pygame.draw.line(screen, (200, 200, 200), (x, 0), (x, screen_h), 1)
+    # Horizontal lines
+    for y in range(0, screen_h, cell_size):
+        pygame.draw.line(screen, (200, 200, 200), (0, y), (screen_w, y), 1)
+
+def run():
+    pygame.init()
+
+    cell_size = 10
+    grid_h, grid_w = field.shape
+    screen_h, screen_w = grid_h*cell_size, grid_h*cell_size
+
+    screen = pygame.display.set_mode((screen_w,screen_h))
+    surface = pygame.surfarray.make_surface(field)
+
+    clock = pygame.time.Clock()
+    running = True
+    step = 0
+
+    while running:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+
+        """TODO Dynamic stuff here"""
+        step += 1
+
+        """Convert map to rbg colors and display it"""
+        # Free cells (0) -> white [255,255,255]; walls (1) -> black [0,0,0]
+        rgb = np.zeros((grid_h, grid_w, 3), dtype=np.uint8)  # start black everywhere
+        rgb[field == 0] = [255, 255, 255]                    # paint free cells white
+
+        surface = pygame.surfarray.make_surface(np.transpose(rgb, (1, 0, 2)))
+        scaled_surface = pygame.transform.scale(surface, (screen_w, screen_h))
+
+        # screen.fill((0, 0, 0))               # optional clear (scaled_surface covers it)
+        screen.blit(scaled_surface, (0, 0))    # draw at top-left
+        draw_grid(screen_w, screen_h, cell_size, screen)
+        pygame.display.flip()
+
+        # limit fps
+        clock.tick(30) 
+
+    pygame.quit()
+
+    
 
 test = Enviornment.map()
 hero1 = entity.hero()
 
-map = test.generate_map()
-huh = hero1.gen_path(map)
-test.display_field(huh)
+field = test.generate_map()
+huh = hero1.gen_path(field)
+# test.display_field(huh)
 
 
-
-# print(huh)
+run()
